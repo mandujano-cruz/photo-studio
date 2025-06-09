c406dc28262776e8923c481441476cb245aeb4fe
mandujano_cruz <jesus.mandujano.cruz@outlook.com>
2025-06-09 03:33:19 -0600

    feat: add protected route

---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,11 @@
+import { Navigate, useLocation } from 'react-router-dom';
+
+export default function ProtectedRoute ({ isLoggedIn, children }) {
+  const location = useLocation();
+  const from = location.state?.from || "/"
+  
+  if(isLoggedIn) return <Navigate to={from} />;
+  if(!isLoggedIn) return <Navigate to="/login" state={{from: location}} replace />;
+  return children;
+};
+
