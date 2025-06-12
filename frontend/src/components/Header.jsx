a7f17ce676f3f4d22c329cc6b1b161f508b0cafc
mandujano.cruz <jesus.mandujano.cruz@outlook.com>
2025-06-11 23:49:27 -0600

    feat: se añade selector en header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,15 +3,19 @@ import { NavLink, useLocation } from 'react-router-dom';
 export default function Header () {
   const location = useLocation();
   const isHome = location.pathname === "/";
+  const isPortfolio = location.pathname === "/portfolio";
+  const isServices = location.pathname === "/services";
+  const isAbout = location.pathname === "/about";
+  const isContact = location.pathname === "/contact";
 
   return (
     <header className={`header ${isHome ? 'header_transparent' : 'header_dark'}`}>
       <nav className="header__nav">
-        <NavLink to="/portfolio" className="header__link">PORTAFOLIO</NavLink>
-        <NavLink to="/services" className="header__link">SERVICIOS</NavLink>
+        <NavLink to="/portfolio" className={`header__link ${isPortfolio ? 'header__link_selected' : ''}`}>PORTAFOLIO</NavLink>
+        <NavLink to="/services" className={`header__link ${isServices ? 'header__link_selected' : ''}`}>SERVICIOS</NavLink>
         <NavLink to="/" className="header__logo">Mandujano</NavLink>
-        <NavLink to="/about" className="header__link">NOSOTROS</NavLink>
-        <NavLink to="/contact" className="header__link">CONTACTO</NavLink>
+        <NavLink to="/about" className={`header__link ${isAbout ? 'header__link_selected' : ''}`}>NOSOTROS</NavLink>
+        <NavLink to="/contact" className={`header__link ${isContact ? 'header__link_selected' : ''}`}>CONTACTO</NavLink>
       </nav>
     </header>
   );
