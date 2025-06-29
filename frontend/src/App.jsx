bc3a58261a5a022de0592bc1d844571c3ac65f09
mandujano_cruz <jesus.mandujano.cruz@outlook.com>
2025-06-29 02:04:44 -0600

    feat: se agregan todas las rutas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import { Routes, Route, useNavigate } from 'react-router-dom';
-import { useEffect, useCallback } from 'react';
+import { Routes, Route, useNavigate, Navigate, useLocation } from 'react-router-dom';
+import { useEffect, useCallback, useState } from 'react';
 import Home from './pages/Home';
 import Portfolio from './pages/Portfolio';
 import Services from './pages/Services';
@@ -10,10 +10,31 @@ import Dashboard from './private/Dashboard';
 import ProtectedRoute from './components/ProtectedRoute';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import PrivateHeader from './private/PrivateHeader';
+import Appointments from './private/Appointments'
+import Users from './private/Users/Users';
+import Photo from './private/Photography/Photo';
+import Profile from './private/Profile';
+import Main from './private/Main';
 
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const location = useLocation();
   const navigate = useNavigate();
+
+  const privatePaths = ['/dashboard', '/appointments', '/photography', '/users', '/profile'];
+  const noHeaderFooterPaths = ['/login'];
+
+  const showPublicHeaderFooter = !privatePaths.some(path => location.pathname.startsWith(path)) && !noHeaderFooterPaths.includes(location.pathname);
+  const showPrivateHeader = privatePaths.some(path => location.pathname.startsWith(path));
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+    const redirectPath = location.state?.from?.pathname || "/";
+    navigate(redirectPath);
+  }
+
   const handleKeyDown = useCallback((event) => {
     if ((event.ctrlKey || event.metaKey) && event.key === 'l') {
       event.preventDefault();
@@ -31,24 +52,37 @@ function App() {
 
   return (
     <>
-      <Header/>
+      {showPublicHeaderFooter && <Header/>}
+      {showPrivateHeader && <PrivateHeader/>}
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/portfolio' element={<Portfolio/>} />
         <Route path='/services' element={<Services/>} />
         <Route path='/about' element={<About/>} />
         <Route path='/contact' element={<Contact/>} />
-        <Route path='/login' element={<Login/>} />
+        <Route path='/login' element={<Login handleLogin={handleLogin} />} />
         <Route 
-          path='/portal' 
+          path='/dashboard/*' 
           element={
-            <ProtectedRoute>
-              <Dashboard/>
+            <ProtectedRoute isLoggedIn={isLoggedIn}>
+              <Dashboard />
             </ProtectedRoute>
           } 
+        >
+          <Route index element={<Main />} />
+          <Route path='appointments' element={<Appointments/>} />
+          <Route path='users' element={<Users/>} />
+          <Route path='photo' element={<Photo/>} />
+          <Route path='profile' element={<Profile/>} />
+        </Route>
+        <Route 
+          path="*"
+          element={
+            isLoggedIn ? (<Navigate to="/dashboard" replace />) : (<Navigate to="/login" replace />)
+          }
         />
       </Routes>
-      <Footer/>
+      {showPublicHeaderFooter &&  <Footer/>}
     </>
   );
 }
